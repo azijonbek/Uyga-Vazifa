432b4317732e52accb7c93e8532dc45268fd4ec1
Aziz <azizxolboyev17@gmail.com>
2023-06-05 21:51:02 +0500

    Uyga vazifa bajarildi

---
+++ main.js
@@ -0,0 +1,28 @@
+
+// 4-masala
+
+// let a = 20
+// let b = 4
+// console.log((a + b) / 2 )
+
+
+// 3-masala 
+// let a = 15
+// let b = 7
+// console.log("yig'indisi:"+ a+b)
+// console.log("ko'paytmasi:" + a*b)
+// console.log("a ning kvadrati:" + a*a)
+
+
+
+// 2-masala
+// let a = 20
+// let b = 40
+// console.log(a * b)
+
+
+// 1-masala
+// let a = 5
+// let b = 6
+// let c = 7
+// console.log(a * b * c)
